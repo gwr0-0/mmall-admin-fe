3c61e975750c36d8bd614f1b53441983d5380bf6
gwr0-0 <win101wd@hotmail.com>
2017-12-03 10:51:44 +0800

    user.do

--- src/service/user.jsx
+++ src/service/user.jsx
@@ -34,7 +34,7 @@ export default class User{
     // 登录
     login(userInfo){
         return mm.request({
-            url     : mm.getServerUrl('/manage/user/login.do'),
+            url     : mm.getServerUrl('/user/login.do'),
             method  : 'POST',
             data    : {
                 username : userInfo.username || '',
